c50eb7cea9704ede4ee5b3371e7d733ff8dd07fa
chint-111 <chinguyen.31231027333@st.ueh.edu.vn>
2024-11-08 16:27:22 +0700

    gg

--- proj/session_05.cs
+++ proj/session_05.cs
@@ -1,16 +1,17 @@
+using System.Data;
 using System.Security.Cryptography.X509Certificates;
 
 namespace proj
 {
     internal class session_05
     {
-        public static void Main(string[] args)
+        public static void Main5(string[] args)
         {
             //Exercise1();
             //Exercise2();
-            Exercise3();
-            //Exercise4();
-            //Exercise5();
+            //Exercise3();
+           //Exercise4();
+            Exercise5();
             //Exercise6();
             Console.WriteLine("Press any key to stop");
             Console.ReadKey();
@@ -49,96 +50,113 @@ namespace proj
         }
         public static void Exercise3()
         {
-            Console.WriteLine("Enter a number: ");
-            int a = int.Parse(Console.ReadLine());
-            int d = 0;
-            for (int i = 1; i <= a ; i++)
-            {
-                if (a % i == 0)
-                {
-                    d++;
-                }
-            }
-            if (d == 2)
-            {
-                Console.WriteLine("The num is prime.");
-            }
-            else
-            {
-                Console.WriteLine("The num is not prime.");
-
-            }
-        }
-        public  void Exercise4()
-        {
-            Console.WriteLine("enter the num: ");
+            Console.Write("Enter a number: ");
             int n = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            //1.
-
-            for (int i = 2; i < n; i++)
+            bool Prime = isPrime(n);
+            Console.WriteLine(Prime?$"{n} is prime":$"{n}is not prime");
+            
+        }
+        public static bool isPrime(int n)
+        {
+            if(n<=1)
+                return false;
+            if(n==2||n==3)
+                return true;
+            if(n%2==0||n%3==0)
+                return false;
+            for(int i=5;i<=Math.Sqrt(n);i+=2)
             {
-                int d = 0;
-                for (int j = 1; j <= i; j++)
+                if (n%i==0)
+                    return false ;
+            }
+            return true;
+        }
+        public static void Exercise4()
+        {
+            Console.Write("Enter the number: ");
+            int n=int.Parse(Console.ReadLine());
+            bool Prime= isPr(n);
+            PrimeLess(n);
+            PrintFirstPrime(n);
+        }
+        public static bool isPr(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n == 2 || n == 3)
+                return true;
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+            for (int i = 5; i <= Math.Sqrt(n); i += 2)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static void PrimeLess(int n)
+        {
+            Console.WriteLine($"Prime number less than {n}: ");
+            for (int i=2;i<=n;i++)
+            {
+                if(isPr(i))
                 {
-                    if (i % j == 0)
-                    {
-                        d++;
-                    }
-                }
-                if (d == 2)
-                {
-                    Console.WriteLine($"Prime num less than {n}: {i}");
+                    Console.Write(i+" ");
                 }
             }
             Console.WriteLine();
-            //2.
-
-            int b = 2;
-            int count = 0;
-            while (count < n)
+        }
+        public static void PrintFirstPrime(int n)
+        {
+            Console.WriteLine($"First {n} prime numbers");
+            int c = 0;
+            int a = 2;
+            while(c<n)
             {
-                int c = 0;
-                for (int e = 1; e <= b; e++)
+                if(isPr(n))
                 {
-                    if (b % e == 0)
-                    {
-                        c++;
-                    }
+                    Console.Write(a+" ");
+                    c++;
                 }
-                if (c == 2)
-                {
-                    Console.WriteLine($"The prime number:{b}");
-                    count++;
-                }
-                b++;
+                a++;
             }
         }
-        public  void Exercise5()
+        
+
+        public static void Exercise5()
         {
             Console.WriteLine("enter a number: ");
             int n = int.Parse(Console.ReadLine());
-            if (n<1)
-            {
-                Console.WriteLine("Not perfect.");
-                return;
-            }
-            int sum = 0;
+            bool Perfect = isPerfect(n);
+            PrintAllPerfectNumber();
+        }
+        public static bool isPerfect(int n)
+        {
+            int s = 0;
             for (int i = 1;i<=n/2;i++)
             {
-                if(n%i == 0)
+                if (n%i==0)
                 {
-                    sum+=i;
-                }              
+                    s += i;
+                }
             }
-            if(sum==n)
+            if (s==n) 
+            return true;
+            else return false;  
+        }
+        public static void PrintAllPerfectNumber()
+        {
+            Console.WriteLine("All perfect number less than 1000: ");
+
+            for (int a = 6; a < 1000; a++)
             {
-                Console.WriteLine("This is perfect num.");
+                if (isPerfect(a))
+                {
+                    Console.Write(a+" ");
+                }
             }
-            else 
-            { 
-                Console.WriteLine("Not perfect.");
-            }
+            Console.WriteLine();
+
         }
         public  void Exercise6()
         {
---
+++ proj/session_06.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj
+{
+    internal class session_06
+    {
+        private static void Main (string[] args )
+        {
+            //Exercise01();
+            Exercise02();
+            Console.WriteLine("Press any key to stop");
+            Console.ReadKey();
+        }
+        public static void Exercise01()
+        { 
+            int n =int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+            int[] a = new int[n];
+            SinhMangNgauNhien(a);
+            InMang(a);
+            Sum(a);
+            increaseItems(a, 2);
+            InMang(a);
+            Sum(a);
+        }
+        static void SinhMangNgauNhien(int[] a)
+        {
+            Random random=new Random();
+            for (int i = 0; i< a.Length; i++)
+            {
+               
+                a[i]=random.Next(100); 
+            }
+        }
+        static void InMang(int[]a)
+        {
+            foreach(int item in a)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+        }
+        static void increaseItems(int[] a, int val)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] += val;
+            }
+        }
+        static void Sum(int[] a)
+        {
+            int s = 0;
+            foreach (int item in a)
+            { s += a[item]; }
+            Console.WriteLine($"Sum: {s}");
+        }
+        public static void Exercise02()
+        {
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+            int[] a=new int[n];
+            BienNgauNhien(a);
+            In(a);
+            Average(a);
+
+        }
+        static void BienNgauNhien(int[]a)
+        {
+            Random ran=new Random();
+            for(int i = 0;i< a.Length;i++)
+            {
+                a[i] = ran.Next();
+            }
+        }
+        static void In(int[] a)
+        {
+            foreach (int item in a)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+        }
+        public static void Average(int [] a)
+        {
+            int sum = 0; int count = 0;
+            foreach(int item in a)  
+            {
+                sum += a[item];
+                count++;
+            }
+            double average = sum / count;
+            Console.WriteLine($"The average : {average}");
+        }
+    }
+}
